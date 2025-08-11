4c0a9e6c71f30f3157936f6f2d4e2ec53993175f
N0Xz015 <darkwishper15@gmail.com>
2025-08-12 01:11:13 +0700

    need more fire

--- src/main.c
+++ src/main.c
@@ -2,18 +2,23 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-#define LED_pin 13
+#define LED_pin_1 13
+#define LED_pin_2 12
 
-void app_main(void *pvParams) {
-    esp_rom_gpio_pad_select_gpio(LED_pin);
-    gpio_set_direction(LED_pin, GPIO_MODE_OUTPUT);
-
+void blink(void *pvParameter) {
+    gpio_num_t LED_pin = (gpio_num_t)pvParameter;
     while (1){
         gpio_set_level(LED_pin, 1);
         vTaskDelay(750 / portTICK_PERIOD_MS);
-
         gpio_set_level(LED_pin, 0);
         vTaskDelay(750 / portTICK_PERIOD_MS);
     }
-    
+}
+
+void app_main(void) {
+    gpio_set_direction(LED_pin_1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_pin_2, GPIO_MODE_OUTPUT);
+
+    xTaskCreate(blink, "blink_task_1", 2048, (void *)LED_pin_1, 5, NULL);
+    xTaskCreate(blink, "blink_task_2", 2048, (void *)LED_pin_2, 5, NULL);
 }
