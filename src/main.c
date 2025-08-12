d300838bd8384fdfcf58ae008024597f29f8c32c
N0Xz015 <darkwishper15@gmail.com>
2025-08-12 23:42:55 +0700

    modified

--- src/main.c
+++ src/main.c
@@ -2,23 +2,32 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-#define LED_pin_1 13
-#define LED_pin_2 12
+#define LED_PIN_1 13
+#define LED_PIN_2 12
 
-void blink(void *pvParameter) {
-    gpio_num_t LED_pin = (gpio_num_t)pvParameter;
+void blink_1(void *pvParameter) {
+    gpio_num_t LED_PIN = (gpio_num_t)pvParameter;
     while (1){
-        gpio_set_level(LED_pin, 1);
-        vTaskDelay(750 / portTICK_PERIOD_MS);
-        gpio_set_level(LED_pin, 0);
-        vTaskDelay(750 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+void blink_2(void *pvParameter) {
+    gpio_num_t LED_PIN = (gpio_num_t)pvParameter;
+    while (1){
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void) {
-    gpio_set_direction(LED_pin_1, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_pin_2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_PIN_1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_PIN_2, GPIO_MODE_OUTPUT);
 
-    xTaskCreate(blink, "blink_task_1", 2048, (void *)LED_pin_1, 5, NULL);
-    xTaskCreate(blink, "blink_task_2", 2048, (void *)LED_pin_2, 5, NULL);
+    xTaskCreate(blink_1, "blink_task_1", 2048, (void *)LED_PIN_1, 5, NULL);
+    xTaskCreate(blink_2, "blink_task_2", 2048, (void *)LED_PIN_2, 5, NULL);
 }
